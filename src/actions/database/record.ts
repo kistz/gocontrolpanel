d26a29ed4cf07bdade47f92b04bdfc8922846128
MRegterschot <marijn.regterschot@gmail.com>
2025-04-16 15:59:58 +0200

    feat: sort records by creation date in getRecordCountPerDay function

--- src/actions/database/record.ts
+++ src/actions/database/record.ts
@@ -61,6 +61,7 @@ export async function getRecordCountPerDay(days: number): Promise<
         },
       },
     ])
+    .sort({ createdAt: -1 })
     .toArray();
 
   return records.map((record) => ({
