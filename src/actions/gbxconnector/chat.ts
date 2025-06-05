2afb7c465f132299c72480ad69d4510df5876eb7
MRegterschot <marijn.regterschot@gmail.com>
2025-06-05 22:52:45 +0200

    fix: improve error handling in getChatConfig and updateChatConfig functions

--- src/actions/gbxconnector/chat.ts
+++ src/actions/gbxconnector/chat.ts
@@ -3,20 +3,25 @@ import { ChatConfigSchemaType } from "@/forms/server/interface/chatconfig-schema
 import { doServerActionWithAuth } from "@/lib/actions";
 import { axiosAuth } from "@/lib/axios/connector";
 import { ServerError, ServerResponse } from "@/types/responses";
+import { isAxiosError } from "axios";
 
 export async function getChatConfig(
   serverId: number,
 ): Promise<ServerResponse<ChatConfigSchemaType>> {
   return doServerActionWithAuth(["admin", "moderator"], async () => {
-    const res = await axiosAuth.get<ChatConfigSchemaType>(
-      `chat/${serverId}/config`,
-    );
+    try {
+      const res = await axiosAuth.get<ChatConfigSchemaType>(
+        `chat/${serverId}/config`,
+      );
 
-    if (res.status !== 200) {
-      throw new ServerError("Failed to fetch chat config");
+      return res.data;
+    } catch (error) {
+      const message = isAxiosError(error)
+        ? error.response?.data || "Failed to get chat config"
+        : "Failed to get chat config";
+
+      throw new ServerError(message);
     }
-
-    return res.data;
   });
 }
 
@@ -25,15 +30,19 @@ export async function updateChatConfig(
   config: ChatConfigSchemaType,
 ): Promise<ServerResponse<ChatConfigSchemaType>> {
   return doServerActionWithAuth(["admin"], async () => {
-    const res = await axiosAuth.put<ChatConfigSchemaType>(
-      `chat/${serverId}/config`,
-      config,
-    );
+    try {
+      const res = await axiosAuth.put<ChatConfigSchemaType>(
+        `chat/${serverId}/config`,
+        config,
+      );
 
-    if (res.status !== 200) {
-      throw new ServerError("Failed to update chat config");
+      return res.data;
+    } catch (error) {
+      const message = isAxiosError(error)
+        ? error.response?.data || "Failed to update chat config"
+        : "Failed to update chat config";
+
+      throw new ServerError(message);
     }
-
-    return res.data;
   });
 }
