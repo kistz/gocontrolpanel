446778a678898b719f3bd9eac6417ffb7b3b29f9
MRegterschot <marijn.regterschot@gmail.com>
2025-06-16 13:07:56 +0200

    fix: update RenderInput and QuadComponent for improved handling of number input and visibility attributes

--- src/components/interface/components/quad/quad.tsx
+++ src/components/interface/components/quad/quad.tsx
@@ -21,6 +21,7 @@ const QuadComponent = forwardRef<
   const [attributes, setAttributes] = useState<QuadSchemaType>(
     defaultAttributes ?? {},
   );
+  const [isHovered, setIsHovered] = useState(false);
 
   useImperativeHandle(ref, () => ({
     uuid,
@@ -65,15 +66,35 @@ const QuadComponent = forwardRef<
     >
       <div
         className="w-full h-full"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
         style={{
           zIndex: attributes?.zIndex,
           transform: `scale(${attributes.scale || 1}) rotate(${attributes?.rot || 0}deg) translateX(${getTranslateX(attributes.hAlign)}%) translateY(${getTranslateY(attributes.vAlign)}%)`,
           transformOrigin: "top left",
           display: attributes?.hidden ? "none" : "flex",
           opacity: attributes?.opacity ? 1 : 0,
-          backgroundImage: `url(${attributes?.image || ""})`,
-          backgroundColor: "#" + attributes?.bgColor,
+          backgroundImage: `url(${(isHovered ? attributes.imageFocus : attributes?.image) || ""})`,
+          backgroundSize: "100% 100%",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: isHovered
+            ? "#" + attributes?.bgColorFocus
+            : attributes?.bgColor
+              ? "#" + attributes?.bgColor
+              : "transparent",
           filter: `blur(${attributes?.blurAmount || 0}px)`,
+
+          ...(attributes.alphaMask && {
+            maskImage: `url(${attributes?.alphaMask || ""})`,
+            maskSize: "100% 100%",
+            maskPosition: "center",
+            maskRepeat: "no-repeat",
+            WebkitMaskImage: `url(${attributes.alphaMask})`,
+            WebkitMaskSize: "100% 100%",
+            WebkitMaskPosition: "center",
+            WebkitMaskRepeat: "no-repeat",
+          }),
         }}
       />
     </Rnd>
