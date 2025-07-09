b92fd07e9991070bf7574ef66d1ddac666854f58
MRegterschot <marijn.regterschot@gmail.com>
2025-07-10 00:48:31 +0200

    feat: implement ProjectCard component for displaying Hetzner project details

--- src/app/(gocontroller)/admin/hetzner/page.tsx
+++ src/app/(gocontroller)/admin/hetzner/page.tsx
@@ -1,10 +1,10 @@
 import { getAllHetznerProjects } from "@/actions/database/hetzner-projects";
 import { getAllUsers } from "@/actions/database/users";
+import ProjectCard from "@/components/hetzner/project-card";
 import AddProjectModal from "@/components/modals/add-project";
 import Modal from "@/components/modals/modal";
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
-import { getList } from "@/lib/utils";
 import { IconPlus } from "@tabler/icons-react";
 
 export default async function AdminHetznerPage() {
@@ -31,24 +31,11 @@ export default async function AdminHetznerPage() {
         </Modal>
       </div>
 
-      {projects.map((project) => (
-        <div key={project.id} className="p-4 bg-card rounded-lg shadow">
-          <h2 className="text-xl font-semibold">{project.name}</h2>
-          <p className="text-muted-foreground">
-            Managed by:{" "}
-            {project.users.map((user) => user.user.nickName).join(", ")}
-          </p>
-          <p className="text-sm text-muted-foreground">
-            API Tokens:{" "}
-            {getList(project.apiTokens).length > 0
-              ? getList(project.apiTokens).join(", ")
-              : "None"}
-          </p>
-          <p className="text-sm text-muted-foreground">
-            Last updated: {new Date(project.updatedAt).toLocaleDateString()}
-          </p>
-        </div>
-      ))}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <ProjectCard project={project} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/hetzner/project-card.tsx
@@ -0,0 +1,10 @@
+import { HetznerProjectsWithUsers } from "@/actions/database/hetzner-projects";
+import { Card } from "../ui/card";
+
+export default function ProjectCard({
+  project,
+}: {
+  project: HetznerProjectsWithUsers;
+}) {
+  return <Card></Card>;
+}
