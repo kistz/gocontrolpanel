c70938e3801d766ac69e3bd26b90279dd23e2e1d
MRegterschot <marijn.regterschot@gmail.com>
2025-07-11 16:46:55 +0200

    refactor: enhance getServerTypes function with server action wrapper and improve return type

--- src/actions/hetzner/server-types.ts
+++ src/actions/hetzner/server-types.ts
@@ -1,33 +1,37 @@
+"use server";
+import { doServerActionWithAuth } from "@/lib/actions";
 import { axiosHetzner } from "@/lib/axios/hetzner";
 import { getKeyHetznerServerTypes, getRedisClient } from "@/lib/redis";
 import {
   HetznerServerType,
   HetznerServerTypesResponse,
 } from "@/types/api/hetzner/servers";
-import "server-only";
+import { ServerResponse } from "@/types/responses";
 
 export async function getServerTypes(
   apiToken: string,
-): Promise<HetznerServerType[]> {
-  const redis = await getRedisClient();
-  const key = getKeyHetznerServerTypes();
+): Promise<ServerResponse<HetznerServerType[]>> {
+  return doServerActionWithAuth([], async () => {
+    const redis = await getRedisClient();
+    const key = getKeyHetznerServerTypes();
 
-  const cachedData = await redis.get(key);
-  if (cachedData) {
-    return JSON.parse(cachedData) as HetznerServerType[];
-  }
+    const cachedData = await redis.get(key);
+    if (cachedData) {
+      return JSON.parse(cachedData) as HetznerServerType[];
+    }
 
-  const res = await axiosHetzner.get<HetznerServerTypesResponse>(
-    "/server_types",
-    {
-      headers: {
-        Authorization: `Bearer ${apiToken}`,
+    const res = await axiosHetzner.get<HetznerServerTypesResponse>(
+      "/server_types",
+      {
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+        },
       },
-    },
-  );
+    );
 
-  const serverTypes = res.data.server_types;
-  await redis.set(key, JSON.stringify(serverTypes), "EX", 60 * 60 * 24); // Cache for 24 hours
+    const serverTypes = res.data.server_types;
+    await redis.set(key, JSON.stringify(serverTypes), "EX", 60 * 60 * 24); // Cache for 24 hours
 
-  return serverTypes;
+    return serverTypes;
+  });
 }
