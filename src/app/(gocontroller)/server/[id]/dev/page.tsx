a16557b8a3e1189215d514a80a7f8c54fe967b29
MRegterschot <marijn.regterschot@gmail.com>
2025-07-19 23:57:04 +0200

    feat: redirect to dashboard on live page access

--- src/app/(gocontroller)/server/[id]/dev/page.tsx
+++ src/app/(gocontroller)/server/[id]/dev/page.tsx
@@ -1,4 +1,6 @@
 import DevDashboard from "@/components/dev/dashboard";
+import { routes } from "@/routes";
+import { redirect } from "next/navigation";
 
 export default async function LivePage({
   params,
@@ -6,6 +8,8 @@ export default async function LivePage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+
+  redirect(routes.dashboard);
 
   return <DevDashboard serverId={id} />;
 }
