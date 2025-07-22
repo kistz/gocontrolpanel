b80f71886bbc38900a9160ffda4f0d7e92b0cc05
MRegterschot <marijn.regterschot@gmail.com>
2025-07-22 17:02:52 +0200

    fix: theme-toggle infinite recursion

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -4,25 +4,25 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
 } from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <DropdownMenuItem>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <DropdownMenuSub>
+      <DropdownMenuSubTrigger>
+        <div className="flex items-center gap-2">
+          <Sun className="text-muted-foreground h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="text-muted-foreground absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           Change Theme
-        </DropdownMenuItem>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+        </div>
+      </DropdownMenuSubTrigger>
+      <DropdownMenuSubContent>
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
@@ -32,7 +32,7 @@ export function ThemeToggle() {
         <DropdownMenuItem onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </DropdownMenuSubContent>
+    </DropdownMenuSub>
   );
 }
