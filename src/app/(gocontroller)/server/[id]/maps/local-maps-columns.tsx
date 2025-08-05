33a79cf91b25d749e8a2d73ed81aa46277c38084
MRegterschot <marijn.regterschot@gmail.com>
2025-08-05 23:35:59 +0200

    feat: enhance local maps table with dropdown menu for adding maps and update button with icon

--- src/app/(gocontroller)/server/[id]/maps/local-maps-columns.tsx
+++ src/app/(gocontroller)/server/[id]/maps/local-maps-columns.tsx
@@ -3,9 +3,16 @@
 import { addMap } from "@/actions/gbx/map";
 import { DataTableColumnHeader } from "@/components/table/data-table-column-header";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { getErrorMessage } from "@/lib/utils";
 import { LocalMapInfo } from "@/types/map";
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
 import { useTransition } from "react";
 import { toast } from "sonner";
 import { parseTmTags } from "tmtags";
@@ -75,7 +82,19 @@ export const createColumns = (serverId: string): ColumnDef<LocalMapInfo>[] => [
 
       return (
         <div className="flex justify-end">
-          <Button onClick={handleAddMap}>Add Map</Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={handleAddMap}>
+                Add Map
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       );
     },
