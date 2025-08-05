77881f2a7aaa98911e7fc4fe64277ea1a1d10783
MRegterschot <marijn.regterschot@gmail.com>
2025-08-05 19:19:58 +0200

    feat: added remove all maps button

--- src/components/maps/map-order.tsx
+++ src/components/maps/map-order.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { DndList } from "../dnd/dnd-list";
 import DndListHeaders from "../dnd/dnd-list-headers";
+import ConfirmModal from "../modals/confirm-modal";
 import { Button } from "../ui/button";
 
 export default function MapOrder({
@@ -16,6 +17,8 @@ export default function MapOrder({
   mapList: Maps[];
   serverId: string;
 }) {
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+
   const [defaultMapList, setDefaultMapList] = useState<Maps[]>(mapList);
   const [mapOrder, setMapOrder] = useState<Maps[]>(defaultMapList);
 
@@ -47,14 +50,38 @@ export default function MapOrder({
     }
   }
 
-  async function resetMapOrder() {
+  function resetMapOrder() {
     setMapOrder(defaultMapList);
   }
 
-  async function onRemoveMap(map: Maps) {
+  function onRemoveMap(map: Maps) {
     const newMapOrder = mapOrder.filter((m) => m.uid !== map.uid);
     setMapOrder(newMapOrder);
     setDefaultMapList(newMapOrder);
+  }
+
+  async function removeAllMaps() {
+    try {
+      const filesToRemove = mapOrder
+        .map((m) => m.fileName)
+        .splice(0, mapOrder.length - 1);
+
+      const { error } = await removeMapList(serverId, filesToRemove);
+      if (error) {
+        throw new Error(error);
+      }
+
+      setMapOrder([mapOrder[mapOrder.length - 1]]);
+      setDefaultMapList([mapOrder[mapOrder.length - 1]]);
+      toast.success("All maps succesfully removed", {
+        description:
+          "Did not remove the last map to prevent the server from crashing.",
+      });
+    } catch (error) {
+      toast.error("Error removing maps", {
+        description: getErrorMessage(error),
+      });
+    }
   }
 
   const columns = createColumns(onRemoveMap);
@@ -70,12 +97,25 @@ export default function MapOrder({
           serverId={serverId}
         />
       </div>
-      <div className="flex flex-row-reverse gap-2">
-        <Button onClick={saveMapOrder}>Save Order</Button>
+      <div className="flex gap-2 ml-auto">
+        <Button variant="destructive" onClick={() => setIsConfirmOpen(true)}>
+          Remove All Maps
+        </Button>
+
+        <ConfirmModal
+          isOpen={isConfirmOpen}
+          onClose={() => setIsConfirmOpen(false)}
+          onConfirm={removeAllMaps}
+          title="Remove all maps"
+          description="Are you sure you want to remove all maps? This will not remove the last map to prevent the server from crashing."
+          confirmText="Remove"
+          cancelText="Cancel"
+        />
 
         <Button variant="outline" onClick={resetMapOrder}>
           Reset Order
         </Button>
+        <Button onClick={saveMapOrder}>Save Order</Button>
       </div>
     </div>
   );
